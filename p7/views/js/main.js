1e765c1fc6e5c0b5d163900b6797c59fc38886a2
MutebNidaa <rkrktktk15@gmail.com>
2017-10-24 11:16:29 +0300

    Update main.js

--- p7/views/js/main.js
+++ p7/views/js/main.js
@@ -535,18 +535,20 @@ function updatePositions() {
 window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
+var elem
+var movingPizzas = document.getElementById('movingPizzas1')
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
   for (var i = 0; i < 60; i++) {
-    var elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
     elem.style.height = "100px";
     elem.style.width = "73.333px";
     elem.basicLeft = (i % cols) * s;
     elem.style.top = (Math.floor(i / cols) * s) + 'px';
-    document.querySelector("#movingPizzas1").appendChild(elem);
+    movingPizzas.appendChild(elem);
   }
   updatePositions();
 });
